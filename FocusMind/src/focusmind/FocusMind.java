7111966a38e0e9bfdfd69cfa22be4bc69f7c9913
JFDomChav <JoseFernandoDom@protonmail.com>
2025-03-11 22:25:44 -0600

    El programa hace una lsita de tareas que esta conectada con ActiveRecallManager.

--- FocusMind/src/focusmind/FocusMind.java
+++ FocusMind/src/focusmind/FocusMind.java
@@ -1,40 +1,16 @@
 package focusmind;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class FocusMind {
     public static void main(String[] args) {
         /*
             Siguiente:
                 -Estudiar con active recall
                 -Calendario que muestre cuando y qué debo estudiar
-            El programa checa cada segundo si hay alguna tarea nueva que se deba estudiar
-            y si la hay entonces la agrega al arraylist y notifica a todos
-            los hilos que esperan su respuesta.
-            El programa puede poner mas tiempo cuando la tarea ya fue estudiada
-            y eliminarla cuando alcanza el maximo de iteraciones.
+            El programa hace una lsita de tareas. Falta que el programa
+            elimine las tareas cuando no se ocupen y suba hacia lo mas alto
+            las tareas que ya terminaron su ciclo de espera
         */
-        ActiveRecallManager arm = new ActiveRecallManager(1);
-        
-        Thread hilo = new Thread(){
-            @Override
-            public void run(){
-                while(true){
-                    try {
-                        synchronized(arm){
-                            arm.wait();
-                            // Esperar a que el hilo notifique
-                        }
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(FocusMind.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-            }
-        };
-        arm.start();
-        hilo.start();
-        int id = arm.addTask();
-        System.out.println(id);
+        TasksView tv = new TasksView();
+        tv.setVisible(true);
     }
 }
